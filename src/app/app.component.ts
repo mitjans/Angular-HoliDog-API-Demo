5ede753563c9bb42087032c4b38f1da18873bcc1
Carles Mitjans Coma <carlesm@Carless-MacBook-Pro.local>
2018-12-15 17:35:05 +0100

    Change document title and add document header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HoliDog';
+
+  constructor(private titleService: Title) {
+    titleService.setTitle("Fetch dog images using HoliDog's API!")
+  }
 }
